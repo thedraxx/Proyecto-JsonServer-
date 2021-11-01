2bd039d551f7bf347aac065d2e0739fccbee8004
Frank <55867157+thedraxx@users.noreply.github.com>
2021-11-01 13:54:18 -0300

    Add files via upload

---
+++ js/nuevocliente.js
@@ -0,0 +1,36 @@
+import { mostrarAlerta,validar} from './funciones.js';
+import { nuevoCliente } from './API.js';
+
+
+(function () {
+    const formulario = document.querySelector("#formulario");
+    formulario.addEventListener("submit", validarCliente);
+
+
+    function validarCliente(e) {
+        e.preventDefault();
+        const nombre = document.querySelector("#nombre").value;
+        const email = document.querySelector("#email").value;
+        const telefono = document.querySelector("#telefono").value;
+        const empresa = document.querySelector("#empresa").value;
+
+        const cliente = {
+            nombre,
+            email,
+            telefono, 
+            empresa
+        }
+
+        if (validar(cliente))  //validacion alternativa validacion if
+        {
+            //Mostrar mensaje 
+            mostrarAlerta("todos los campos son obligatorios");
+            return;
+        }
+
+        nuevoCliente(cliente);
+
+        
+    }
+})();
+
